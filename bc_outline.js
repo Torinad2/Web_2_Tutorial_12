1c99256d19a1da244df396745eec9666e01b9867
Torinad2 <157666959+Torinad2@users.noreply.github.com>
2025-11-15 06:44:49 -0500

    2

--- bc_outline.js
+++ bc_outline.js
@@ -23,7 +23,71 @@
       list items are appended to TOCList,
       the list items are based on the element names
       specified in the headings array
-
-
 */
 
+/* Generate an outline based on h1 through h6 headings in the source document */
+
+window.addEventListener("load", makeOutline);
+
+function makeOutline() {
+    // location of the document outline
+    var outline = document.getElementById("outline");
+
+    // Source document for the outline
+    var source = document.getElementById("doc");
+
+    var mainHeading = document.createElement("h1");
+    var outlineList = document.createElement("ol");
+    var headingText = document.createTextNode("Outline");
+
+    mainHeading.appendChild(headingText);
+    outline.appendChild(mainHeading);
+    outline.appendChild(outlineList);
+
+    createList(source, outlineList);
+
+}
+
+function createList(source, outlineList) {
+    // Headings for the outline
+    var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+
+    //Previous level of the heading
+    var prevLevel = 0;
+
+    // Running total of the article headings
+    var headNum = 0;
+
+
+    /* Loop through all of the child nodes of source
+       article until no child nodes are left */
+
+    for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+        // Examine only article headings
+        var headLevel = headings.indexOf(n.nodeName);
+
+        if (headLevel !== -1) {
+            // Add an id to the heading if it is missing
+            headNum++;
+            if (n.hasAttribute("id") === false) {
+                n.setAttribute("id", "head" + headNum);
+            }
+
+            var listElem = document.createElement("li");
+
+            //listElem.innerHTML = n.firstChild.nodeValue;
+
+            // Create hypertext links to the document headings
+            var linkElem = document.createElement("a");
+            linkElem.innerHTML = n.innerHTML;
+            linkElem.setAttribute("href", "#" + n.id);
+
+            // Append the hypertext link to the list item
+            listElem.appendChild(linkElem);
+
+            outlineList.appendChild(listElem);
+
+        }
+
+    }
+}
